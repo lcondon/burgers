5c8cf7ba1f801743d8d7ff713638aa4dafb2f4de
Lucas Condon <lcondon@bennington.edu>
2018-08-07 12:41:23 -0400

    sqlz

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": "march2195",
+      "database": "burgerDB",
+      "host": "localhost",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": "march2195",
+      "database": "burgerDB",
+      "host": "localhost",
+      "dialect": "mysql"
+    },
+    "production": {
+    'use_env_variable': 'JAWSDB_URL',
+      "dialect": "mysql"
+    }
+  }
+  
--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/config.js')[env];
 
-var sequelize = new Sequelize("burgerDB", "root", "march2195", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize("burgerDB", "root", "march2195", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+};
 // Exports the connection for other files to use
 module.exports = sequelize;
