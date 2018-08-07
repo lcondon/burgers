15257feef6869072cac5d1d6f3d73a973917c975
Lucas Condon <lcondon@bennington.edu>
2018-08-07 13:21:55 -0400

    sqlz

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var Sequelize = require("sequelize");
+var sequelize;
 
-
-
-var sequelize = new Sequelize("y2np4ilf3ui4fifo", "xur6n0hr2hg8t4wac", "tfcnqu8zqfqkdc23", {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
     host: "a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
+    port: 22,
     dialect: "mysql",
     pool: {
       max: 5,
@@ -12,7 +12,18 @@ var sequelize = new Sequelize("y2np4ilf3ui4fifo", "xur6n0hr2hg8t4wac", "tfcnqu8z
       idle: 10000
     }
   })
-
+} else {
+  sequelize = new Sequelize("burgerDB", "root", "march2195", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+};
 // Exports the connection for other files to use
 module.exports = sequelize;
 
