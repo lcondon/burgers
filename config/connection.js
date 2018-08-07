258447248d61a07b951c331ae647708f82dfe698
Lucas Condon <lcondon@bennington.edu>
2018-08-07 12:48:16 -0400

    sqlz

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,16 @@ var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/config.js')[env];
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize("y2np4ilf3ui4fifo", "xur6n0hr2hg8t4wac", "tfcnqu8zqfqkdc23", {
+    host: "a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  })
 } else {
   var sequelize = new Sequelize("burgerDB", "root", "march2195", {
     host: "localhost",
@@ -18,3 +27,4 @@ if (config.use_env_variable) {
 };
 // Exports the connection for other files to use
 module.exports = sequelize;
+
